022d046560b2f4b324bf2842fc6b51b778642cce
Abiola Badaru <abdulbasitalism@gmail.com>
2023-11-02 19:30:45 -0700

    my first task commit message on task 0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,15 @@
 /**
  * *malloc_checked - writing a function that allocates memory using malloc.
  * @b: The number of bytes to the allocated memory
+ *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
 	void *y;
+
 	y = malloc(b);
-	if (y == NULL);
+	if (y == NULL)
 	exit(98);
 	return (y);
 }
