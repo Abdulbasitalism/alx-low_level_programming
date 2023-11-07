2a67c9e0ce945609b2d303b72349da680cea77ce
Abiola Badaru <abdulbasitalism@gmail.com>
2023-11-06 17:26:37 -0800

    my next task on 2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - the name of the file it was compiled from
+ *main - the name of the file it was compiled from.
  *
- * Return: Always 0 (Success)
+ *Return: 0.
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
