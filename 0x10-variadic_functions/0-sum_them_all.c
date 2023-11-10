238e045c6fb807cbace711b66408c6f7a98b50c8
Abiola Badaru <abdulbasitalism@gmail.com>
2023-11-09 19:00:51 -0800

    my task o

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,21 +7,17 @@
  * @...: ellipsis which stands for the variable number of parameters to calc. sum.
  * Return: 0 if n == 0.
  */
-int sum_them_all(const unsigned int n, ...){
-	if (n == 0) {
-		return 0;
-}
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i, sum = 0;
 
-int sum = 0;
+	va_start(nums, n); // initialize argument
 
-va_list args;
-va_start(args, n); /* initialize the arguement */
+	for (i = 0; i < n; i++)
+		sum += va_arg(nums, int);
 
-for (unsigned int i = 0; i < n; ++i) {
-	sum += va_arg(args, int);
-}
+	va_end(nums); // clean up
 
-va_end(args); /* clean up */
-
-return sum;
+	return (sum);
 }
